3f7fb7ead36aa586eff738026f2d6a2e25372955
jules <jules.cayot@ik.me>
2024-04-13 14:37:24 +0300

    Mouais

--- srcs/commands/ducks_handling.c
+++ srcs/commands/ducks_handling.c
@@ -27,12 +27,21 @@ int	open_put_error(char *file, int oflag)
 	int	fd;
 	int	error;
 
-	if (access(file, W_OK) != 0 || unlink(file) == 0)
+	if (oflag == O_RDONLY)
+		fd = open(file, oflag);
+	else
 	{
+		if (access(file, F_OK) == 0)
+		{
+			if (oflag == (O_CREAT | O_WRONLY))
+				unlink(file);
+		}
+		else
+			oflag = O_CREAT | O_WRONLY;
 		fd = open(file, oflag, 420);
-		if (fd != -1)
-			return (fd);
 	}
+	if (fd != -1)
+		return (fd);
 	error = errno;
 	return (put_file_error(file, strerror(error)));
 }
